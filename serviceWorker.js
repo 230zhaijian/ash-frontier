f6b74d72af40f62037a4308d249e95852e26089a
230zhaijian <zhaijian230@gmail.com>
2025-11-04 16:15:23 +0900

    Update and rename AshFrontier_serviceWorker.js to serviceWorker.js

---
+++ serviceWorker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME='ash-frontier-cache-v2';
+const URLS_TO_CACHE=[
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', event=>{
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache=>cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event=>{
+  event.respondWith(
+    caches.match(event.request).then(resp=>resp||fetch(event.request))
+  );
+});
